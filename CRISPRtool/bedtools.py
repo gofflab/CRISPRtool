36fb00de3ba7e6a27908c70021dd1497f994a01c
Loyal A. Goff <loyalgoff@gmail.com>
2014-03-31 15:28:29 -0400

    Added blank file for bedtools overlap logic

---
+++ CRISPRtool/bedtools.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+####################
+# Check for gene overlap with Bedtools
+####################
+
+#
+bedtoolsBin = ''
+refGTF = ''
+
+
+def findOverlap(guides,GTFfile):
+	pass
+
+
+def parseOverlap(guides,overlapResult):
+	pass
